d422757a1dbc81c47e241475787781b97b0f5311
davidajimati <ajimatidavid@gmail.com>
2023-05-16 12:00:56 +0100

    updates

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python3
+'''
+compresses all file in current directory into the 'versions' dir
+creates the target folder is not exists
+'''
 from datetime import datetime
-from fabric import Connection
+from fabric.api import local
+import os.path
+
 
 def do_pack():
+    '''
+    function to handle whole process
+    '''
     # get current datetime in string format
     cur = datetime.now().strftime("%Y%m%d%H%M%S")
-    cur = "web_static_" + cur
-    # connect to localhost
-    conn = Connection('localhost')
+    file = "web_static_" + cur
 
     # connect and run command
-    conn.local("mkdir -p versions")
-    conn.local("tar -czvf versions/{}.tgz *".format(cur))
+    local("mkdir -p versions")
+    local("tar -czvf versions/{}.tgz *".format(file))
+
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {}".format(cur)).failed is True:
+        return None
+    return file
