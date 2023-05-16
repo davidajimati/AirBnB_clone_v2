4449f796a710f1ed53a6310cf4fd6b7e077f8c2e
davidajimati <ajimatidavid@gmail.com>
2023-05-16 08:56:25 +0100

    sudo commands removed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,32 +2,32 @@
 # script that sets up your web servers for the deployment of web_staticscript that sets up your web servers for the deployment of web_staticscript that sets up your web servers for the deployment of web_staticscript that sets up your web servers for the deployment of web_staticscript that sets up your web servers for the deployment of web_staticscript that sets up your web servers for the deployment of web_staticscript that sets up your web servers for the deployment of web_staticscript that sets up your web servers for the deployment of web_staticscript that sets up your web servers for the deployment of web_staticscript that sets up your web servers for the deployment of web_staticscript that sets up your web servers for the deployment of web_static
 
 # install nginx
-sudo apt-get update
-sudo apt-get install nginx
+apt-get update
+apt-get install nginx
 
 # create necessary directories
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
+mkdir -p /data/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+touch /data/web_static/releases/test/index.html
 
 # create a test html file to confirm nginx configuration
 printf %s "
 Your nginx is workin correctly
-" | sudo tee -a /data/web_static/releases/test/index.html > /dev/null
+" | tee -a /data/web_static/releases/test/index.html > /dev/null
 
 # create symbolic link
 link_path="/data/web_static/current"
 target_path="/data/web_static/releases/test"
 
 if [ -L "$link_path" ]; then
-    sudo rm "$link_path"
+    rm "$link_path"
 fi
-sudo ln -s "$target_path" "$link_path"
+ln -s "$target_path" "$link_path"
 
 # change folder user and groub to ubuntu
-sudo chown -R 1000:1000 /data/
+chown -R 1000:1000 /data/
 
 # configure nginx to serve hbnb static
 
@@ -50,7 +50,7 @@ printf %s "server {
     location = /etc/nginx/html/404.html {
       internal;
     }
-}" | sudo tee -a /etc/nginx/sites-available/default > /dev/null
+}" | tee -a /etc/nginx/sites-available/default > /dev/null
 
 # restart nginx
-sudo service nginx restart
+service nginx restart
