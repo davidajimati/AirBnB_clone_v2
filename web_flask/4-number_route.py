46b64872433b52d1c1c8a28ede19453304ff7ca2
davidajimati <ajimatidavid@gmail.com>
2023-05-25 10:28:58 +0100

    checking if n is anumber

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+'''
+starts a Flask web application:
+    displays "Hello HBNB on port 5000
+    "HBNB" on /hbnb
+    /c/<text>: display “C ” followed by the value of
+        the text variable (replace underscore _ symbols
+        with a space )
+'''
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    '''Displays "Hello HBNB" '''
+    return "Hello HBNB"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''Displays "HBNB" '''
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    '''Displays "C and text" '''
+    res = text.replace("_", " ")
+    return ("C {}".format(res))
+
+
+@app.route('/python/', defaults={'text': ''}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text):
+    '''Displays "python is cool" -> by default
+    or python (followed by text)
+    '''
+    if text is not '':
+        res = text.replace("_", " ")
+        return ("Python {}".format(res))
+    return "Python is cool"
+
+@app.route('/number/<n>', strict_slashes=False)
+def number_route(n):
+    '''Displays "n is a number if it is" '''
+    if type(n) is 'int':
+        return ("{} is a number".format(n))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
